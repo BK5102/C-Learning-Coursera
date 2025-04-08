1052a853566daeb03727f7d3247d6a32545bd8f5
Bhavana Kannan <bhavanakannan06@gmail.com>
2025-04-08 13:42:15 -0700

    graded assignment and poem

---
+++ Coding_for_Everyone/project1/poem.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+int main(void)
+{
+    printf("Pain enters through an open window\n"); 
+    printf("and you say it is the wind.\n"); 
+    printf("It marrows into beams, gutters.\n");
+    printf("walls. Still…\n"); 
+
+    return 0;
+}
