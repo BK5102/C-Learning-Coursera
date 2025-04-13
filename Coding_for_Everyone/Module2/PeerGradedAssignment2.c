2d9d58a533ccaa6381e5131f55469ae3decfeeb1
Bhavana Kannan <bhavanakannan06@gmail.com>
2025-04-12 17:05:27 -0700

    path and complier problems fixed

---
+++ Coding_for_Everyone/Module2/PeerGradedAssignment2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+
+
+int main(){
+    double value;
+
+    std::cout << "Enter a number between 0 and 1: ";
+    std::cin >> value;
+
+    if (value > 0 && value < 1){
+        double result = std::sin(value);
+        std::cout << "sine(" << value << ") = " << result << std::endl;
+
+    }
+    else {
+        std::cout << "Error: must be between 0 and 1 (exclusive)." << std::endl;
+
+    }
+
+
+    return 0;
+
+}
